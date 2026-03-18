cba62d808df290be0b6eec6f168dce8df1391dc2
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-18 09:46:31 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['ecommerce-backend-production-aa2e.up.railway.app'], // domain backend
+  },
 };
 
 export default nextConfig;
