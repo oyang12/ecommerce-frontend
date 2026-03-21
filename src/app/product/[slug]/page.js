f9d5395ece611679cd30985de947d96bc763ab11
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-21 12:04:15 +0700

    Update page.js

--- src/app/product/[slug]/page.js
+++ src/app/product/[slug]/page.js
@@ -207,8 +207,7 @@ export default function ProductDetailPage() {
                   disabled={product.stock <= 0}
                   className="w-full bg-[#0a0f1e] text-white py-4 rounded-2xl font-black uppercase text-xs tracking-[0.2em] hover:bg-blue-700 transition-all shadow-xl active:scale-95 disabled:bg-gray-300 disabled:cursor-not-allowed"
                 >
-                  Tambah ke Cart
-                    {isLoggedIn ? "Tambah ke Cart" : "Login untuk Beli"}
+                  {isLoggedIn ? "Tambah ke Cart" : "Login untuk Beli"}
                 </button>
               </div>
             </div>
